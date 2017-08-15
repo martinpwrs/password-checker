63097d94c098b63781ff770eca8464112e48ed05
Martin Powers <martinpwrs@gmail.com>
2017-08-15 14:30:39 -0600

    Commit after finishing spring-angularjs-tutorial-3 by chrishenkel Jackson and JSON testing.

---
+++ src/main/java/password/mvc/BlogEntryController.java
@@ -0,0 +1,20 @@
+package password.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import password.entities.BlogEntry;
+
+@Controller
+public class BlogEntryController {
+
+    @RequestMapping(value="/test", method = RequestMethod.POST)
+    public @ResponseBody BlogEntry test(@RequestBody BlogEntry entry)
+    {
+        return entry;
+    }
+}
